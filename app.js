fafa638df735a005b6527c38a57feaeb3a3873c7
mohib181 <mohib18169@gmail.com>
2021-05-03 05:44:11 +0600

    trying to solve get / method attemp 3

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
+const app = express();
+
+dotenv.config( { path : 'config.env'} );
+const PORT = process.env.PORT || 8080;
+
+// log requests
+app.use(morgan('tiny'));
+
+// mongodb connection
+connectDB();
+
+// parse request to body-parser
+//app.use(bodyparser.urlencoded({ extended : true}));
+app.use(express.json());
+
+// set view engine
+app.set("view engine", "ejs");
+//app.set("views", path.resolve(__dirname, "views/ejs"))
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+
+// test start purpose
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+const ownerController = require('./server/controller/ownerController');
+app.get('/api/owner/getAll', ownerController.getAllOwners); //this will be removed afterwards
+
+// test end
+
+/*if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'index.html'));
+});*/
+
+
+// load routers
+app.use('/', require('./server/routes/router'));
+
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
